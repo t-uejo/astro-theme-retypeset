80c6e67860c846c5c23328690f3da7d3bf1e6566
t-uejo <tiki.ur.tiki@icloud.com>
2025-06-08 13:32:54 +0900

    fix: copyright修正

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const themeConfig: ThemeConfig = {
     // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
     i18nTitle: true, // true, false
     // author name
-    author: 'Taiki Uejo',
+    author: 'Uejo',
     // site url
     url: 'https://github.com/t-uejo',
     // favicon url
